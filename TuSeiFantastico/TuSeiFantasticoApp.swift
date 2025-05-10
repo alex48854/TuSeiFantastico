213df3cf5a017ee6b80a383474dc3788f899028c
Alessandro Gaspari <alessandrogaspari48@gmail.com>
2025-05-10 19:24:03 +0200

    Initial Commit

---
+++ TuSeiFantastico/TuSeiFantasticoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuSeiFantasticoApp.swift
+//  TuSeiFantastico
+//
+//  Created by Alessandro Gaspari on 10/05/25.
+//
+
+import SwiftUI
+
+@main
+struct TuSeiFantasticoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
