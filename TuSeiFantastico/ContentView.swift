0c2ae04d578d57691a5969cbbbaed5ce740d54aa
Alessandro Gaspari <alessandrogaspari48@gmail.com>
2025-05-14 18:18:20 +0200

    Aggiornamenti dopo la prima lezione

--- TuSeiFantastico/ContentView.swift
+++ TuSeiFantastico/ContentView.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            Text("Tu Sei Fantastico!")
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+                .foregroundColor(Color.red)
         }
         .padding()
     }
@@ -22,3 +26,14 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+/* .resizable() allarga la figura in tutto lo spazio disponibile
+// .scaledToFit() adatta la figura senza che questa venga distorta
+// .foregroundStyle cambia il colore dell'immagine.
+    Questo vale anche per i testi.
+    I nomi delle immagini DEVONO essere SEMPRE scritti in minuscolo.
+    Per mettere un simbolo come immagine, si può sceglierlo dalla libreria.
+    
+ 
+ */
+    
